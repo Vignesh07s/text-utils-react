c267066d2780dda9b8d853d662044d9e5dc9e97d
VIGNESHWARA REDDY SIDDIREDDY <135433437+Vignesh07s@users.noreply.github.com>
2024-09-09 12:43:58 +0530

    Updated navbar.js for mobile and small screen compatibility

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -35,6 +35,9 @@ export default function NavBar() {
     <nav className="navbar navbar-expand-lg" style={{'backgroundColor':color}}>
       <div className="container-fluid">
         <a className="navbar-brand" href="/">TextUtilsApp</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
